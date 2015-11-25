655e9902a91867020cce57f56d4d719ee1026ec8
thisjohn <thisjohn19@gmail.com>
2015-11-26 02:49:33 +0800

    Use 'hello-jni'

--- app/src/main/java/com/sclee/android/hello/MainActivityFragment.java
+++ app/src/main/java/com/sclee/android/hello/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.sclee.android.hellojni.HelloJni;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView helloTextView = (TextView) rootView.findViewById(R.id.helloTextView);
+
+        // Use hello-jni
+        HelloJni helloJni = new HelloJni();
+        helloTextView.setText(helloJni.helloFromNative());
+
+        return rootView;
     }
 }
